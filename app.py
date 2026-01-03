0b546444aee5e638afb16f1cb54e3e4a83aee268
panditpooja <poojapanditnayak@gmail.com>
2026-01-02 21:53:22 -0700

    Added RAG workflow and added web app features for the chatbot

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from langchain_pinecone import PineconeVectorStore
 from langchain_openai import ChatOpenAI
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
+# from langchain.chains import ConversationChain
+# from langchain.chains.conversation.memory import ConversationBufferMemory
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 from src.prompt import *
 import os
-
 
 app = Flask(__name__)
 
@@ -16,7 +17,6 @@ load_dotenv()
 
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
-
 
 embeddings = download_hugging_face_embeddings()
 
@@ -28,10 +28,16 @@ docsearch = PineconeVectorStore.from_existing_index(
     embedding=embeddings
 )
 
-
 retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
 
-chatModel = ChatOpenAI(model="gpt-4o")
+# Defining the model
+llm = ChatOpenAI(
+    model="google/gemma-3-27b-it:free",  
+    base_url="https://openrouter.ai/api/v1",
+    api_key=OPENROUTER_API_KEY,
+    temperature = 0
+)
+
 prompt = ChatPromptTemplate.from_messages(
     [
         ("system", system_prompt),
@@ -39,16 +45,17 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-question_answer_chain = create_stuff_documents_chain(chatModel, prompt)
+# conversation_buf = ConversationChain(
+#     llm=llm,
+#     memory=ConversationBufferMemory()
+# )
+
+question_answer_chain = create_stuff_documents_chain(llm, prompt)
 rag_chain = create_retrieval_chain(retriever, question_answer_chain)
-
-
 
 @app.route("/")
 def index():
     return render_template('chat.html')
-
-
 
 @app.route("/get", methods=["GET", "POST"])
 def chat():
@@ -58,8 +65,6 @@ def chat():
     response = rag_chain.invoke({"input": msg})
     print("Response : ", response["answer"])
     return str(response["answer"])
-
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port= 8080, debug= True)
